e737163880dbbc514f57040a12a3e6442993189d
三國直樹 <nmikuni@mikuninaokis-MacBook.local>
2018-07-22 18:22:10 +0900

    top+Seafood designed

--- app/wedding/app/controllers/welcome_controller.rb
+++ app/wedding/app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
     def show
         @callmessage = Manage.find_by(code: params[:code]).top
+        @callname = Manage.find_by(code: params[:code]).name
     end
 end
